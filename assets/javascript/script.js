fcb1539aa1ad0da7a8a671c836c9dda6bb31bb83
Jake Krayger <jkrayg@protonmail.com>
2020-04-04 22:02:46 -0400

    Adding login to firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
-    $('.carousel').carousel({
-      interval: 4000
+  var firebaseConfig = {
+    apiKey: "AIzaSyC-XewrIouPXycLdTRf30vAk2cLgMr4y8U",
+    authDomain: "login-layout-150ad.firebaseapp.com",
+    databaseURL: "https://login-layout-150ad.firebaseio.com",
+    projectId: "login-layout-150ad",
+    storageBucket: "login-layout-150ad.appspot.com",
+    messagingSenderId: "179769582467",
+    appId: "1:179769582467:web:f4147acd6d14bfb5a38cb6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  $('.carousel').carousel({
+    interval: 4000
+  })
+  
+
+  $("#submit-login").on("click", function() {
+    var username = $("#email-input").val().trim();
+    var password = $("#password-input").val().trim();
+
+    database.ref().set({
+      Password: password,
+      Username: username
     })
-  });
+  })
+  
+
+});
